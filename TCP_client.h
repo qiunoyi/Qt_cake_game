aab20df5b442912f5d130213df89f8de82db5aa1
qiunoyi <18359778223@163.com>
2021-10-01 19:10:45 +0800

    master

--- TCP_client.h
+++ TCP_client.h
@@ -8,14 +8,31 @@ class TCP_client : public QObject
 public:
     TCP_client(unsigned short p, QString i) : port(p), ip(i)
     {
-        
     }
-    void connect()
+    void connect_Host()
     {
         m_tcp = new QTcpSocket(this);
         m_tcp->connectToHost(QHostAddress(ip), port);
-        
+        connect(m_tcp, &QTcpSocket::disconnected, this, [=]() 
+        {
+            m_tcp->close();
+            m_tcp->deleteLater();
+        });
     }
+    void send(QTcpSocket *tcp)
+    {
+        tcp->write("");
+        //.......
+    }
+    void read(QTcpSocket *tcp)
+    {
+        connect(tcp,&QTcpSocket::readyRead,this,[=]()
+        {
+            auto data=tcp->readAll();
+            //......
+        });
+    }
+    
 
 private:
     QTcpSocket *m_tcp;
